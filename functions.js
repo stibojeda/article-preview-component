260be329212ce3a1e7f67b46e970a9467577a28f
Stiven Ojeda <ojeda74stiven@gmail.com>
2022-12-14 13:03:21 -0500

    The project was finished. Exists errors in JS code and the CSS has a lot of repeated code. The functionality is the same as in frontendmentor design, almost.

--- functions.js
+++ functions.js
@@ -1,23 +1,43 @@
 window.onload = () => {
 
+  let componentBox = document.querySelector(".component");
   let shareButton = document.getElementById("share-button");
   let shareIconNormal = document.querySelector(".share-icon");
   let shareIconActive = document.querySelector(".share-icon--active");
   let boxActive = document.querySelector('.profile-block__share-active');
-  let active = false;
+  let networksBox = document.querySelector(".share-box");
+  let profileBlock = document.querySelector(".profile-block")
+  let authorAvatar = document.querySelector(".profile-block__avatar");
+  let informationProfile = document.querySelector(".profile-block__information");
+  let boxActiveMobile = document.querySelector(".share-active-mobile");
+  let shareButtonMobile = document.querySelector(".mobile-button-share");
+  console.log(document.body.clientWidth);
 
+  if (document.body.clientWidth < 900) {
+    shareButton.addEventListener("click", () => {
+      profileBlock.style.display = "none";
+      boxActiveMobile.style.display = "block";
+    });
 
-  shareButton.addEventListener("click", () => {
-    boxActive.style.display = "block";
-    shareIconNormal.style.display = "none";
-    shareIconActive.style.display = "block";
-  });
+    shareButtonMobile.addEventListener("focusout", () => {
+      profileBlock.style.display = "flex";
+      boxActiveMobile.style.display = "none";
+    })
+  }
+  if (document.body.clientWidth >= 900) {
+    shareButton.addEventListener("click", () => {
+      boxActive.style.display = "block";
+      shareIconNormal.style.display = "none";
+      shareIconActive.style.display = "block";
 
-  shareButton.addEventListener("focusout", () => {
-    boxActive.style.display = "none";
-    shareIconNormal.style.display = "block";
-    shareIconActive.style.display = "none";
-  })
+    });
   
+    shareButton.addEventListener("focusout", () => {
+      boxActive.style.display = "none";
+      shareIconNormal.style.display = "block";
+      shareIconActive.style.display = "none";
+    });  
+  }
+ 
 
 }
